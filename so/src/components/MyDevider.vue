c74162794545380c0f055e2a132cad88a7d41bff
RiceM001 <13532153969@163.com>
2024-05-14 18:42:15 +0800

    first commit2

---
+++ so/src/components/MyDevider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="my-devider" />
+</template>
+
+<style scoped>
+.my-devider {
+  margin-bottom: 16px;
+}
+</style>
---
+++ springboot-init-master/src/main/java/com/yupi/springbootinit/controller/SearchContorller.java
@@ -0,0 +1,19 @@
+package com.yupi.springbootinit.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+@Slf4j
+public class SearchContorller {
+
+//    @Resource
+//    private PictureService pictureService;
+//
+//    @PostMapping("/all")
+//    public List<SearchVO> searchAll(SearchResult searchResult){
+//
+//    }
+}
